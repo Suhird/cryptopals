eee7049ea96852557902466cf82fda430caeffe9
suhird <suhirdsingh92@gmail.com>
2021-05-15 21:58:22 -0400

    Added input as command line argument

--- hex_to_base64.py
+++ hex_to_base64.py
@@ -1,5 +1,13 @@
-#this program converts hex to base64
+## This program converts hex to base64
+## input: hex string
+## output: base64 encoded string
+## usage:-
+## >>> python hex_to_base64.py [HEX STRING]
+##
+
 from base64 import b64encode
+import sys
+
 
 def hex_to_byte(s):
     arr = bytes.fromhex(s)
@@ -7,5 +15,6 @@ def hex_to_byte(s):
 
 if __name__ == '__main__':
     test = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    byte_arr = hex_to_byte(test)
+    input_str = sys.argv[1]
+    byte_arr = hex_to_byte(input_str)
     print(b64encode(byte_arr), end=' ')
